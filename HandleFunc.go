508e92eceff010dbfa74ca8781776ecc02cafd1c
MYK12397 <yusufzai.12397@gmail.com>
2021-09-21 11:28:25 +0530

    HandleFunc

---
+++ HandleFunc.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+
+
+func d(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w,"doggy doggy doggy")
+}
+
+
+func c(w http.ResponseWriter, r *http.Request) {
+
+		io.WriteString(w,"meow meow meow")
+	
+}
+
+func main() {
+
+	http.HandleFunc("/dog/",d)
+	http.HandleFunc("/cat",c)
+	http.ListenAndServe(":8080", nil)
+
+
+}
